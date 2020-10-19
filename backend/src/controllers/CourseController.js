bc7ad8e7432f4a4d4d8667553437ccf68dfce7d2
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-18 23:27:06 -0300

    handling exceptions: Controllers

--- backend/src/controllers/CourseController.js
+++ backend/src/controllers/CourseController.js
@@ -8,22 +8,43 @@ const create = async (request, response) => {
     // Utilizando o cabeçalho da requisição para verificar quem é o responsável por esse curso
     const university_id = request.headers.authorization
 
-    // Realizando um destruction no array resultado da inserção para pegar o id gerado
-    const [id] = await connection('course').insert({
-        name,
-        email,
-        description,
-        score,
-        university_id
-    })
+    // Verificando integridade dos dados
+    if (!name) return response.status(400).json({ error: 'Nome inválido' })
+    if (!email) return response.status(400).json({ error: 'Email inválido' })
+    if (!description) return response.status(400).json({ error: 'Descrição inválida' })
+    if (!(score > 1 && score <= 5)) return response.status(400).json({ error: 'Pontuação inválida' })
+    if (!university_id) return response.status(401).json({ error: 'Não autorizado' })
 
-    // Retornando o ID como resposta 
-    return response.json({ id })
+    try {
+
+        // Realizando um destruction no array resultado da inserção para pegar o id gerado
+        const [id] = await connection('course').insert({
+            name,
+            email,
+            description,
+            score,
+            university_id
+        })
+
+        // Retornando o ID como resposta 
+        return response.json({ id })
+
+    } catch {
+
+        return response.status(500).json({ error: 'Falha ao criar' })
+
+    }
 }
 
 const list = async (request, response) => {
-    const courses = await connection('course').select('*')
-    return response.json(courses)
+    try {
+        // Buscando lista de cursos
+        const courses = await connection('course').select('*')
+        return response.json(courses)
+
+    } catch {
+        return response.status(500).json({ error: 'Falha ao buscar' })
+    }
 }
 
 const remove = async (request, response) => {
@@ -34,21 +55,39 @@ const remove = async (request, response) => {
     // Utilizando o cabeçalho da requisição para verificar quem é o responsável por esse curso
     const university_id = request.headers.authorization
 
-    const courses = await connection('course')
-        .where('id', id)  // Comparando o ID escolhido com o do banco
-        .select('university_id') // Pegando o ID da universidade responsável
-        .first() // Pegando o primeiro que ele encontrar
+    // Verificando integridade dos dados
+    if (!id) return response.status(400).json({ error: 'Necessita de um ID' })
+    if (!university_id) return response.status(401).json({ error: 'Não autorizado' })
 
-    // Verificando se o ID da requisição é o mesmo ID do responsável pelo curso (EVITAR QUE UMA UNIVERSIDADE EXCLUA O CURSO DE OUTRA)
-    if (university_id != courses.university_id) {
-        return response.status(401).json({ error: 'Operação não permitida' })
+    try {
+
+        const courses = await connection('course')
+            .where('id', id)  // Comparando o ID escolhido com o do banco
+            .select('university_id') // Pegando o ID da universidade responsável
+            .first() // Pegando o primeiro que ele encontrar
+
+        // Verificando se o ID da requisição é o mesmo ID do responsável pelo curso (EVITAR QUE UMA UNIVERSIDADE EXCLUA O CURSO DE OUTRA)
+        if (university_id != courses.university_id) return response.status(401).json({ error: 'Operação não permitida' })
+
+    } catch {
+
+        return response.status(500).json({ error: 'Falha ao buscar' })
+
     }
 
-    await connection('course')
-        .where('id', id)  // Comparando o ID escolhido com o do banco
-        .delete() // Deletando o curso no banco
+    try {
 
-    return response.status(204).send()
+        await connection('course')
+            .where('id', id)  // Comparando o ID escolhido com o do banco
+            .delete() // Deletando o curso no banco
+
+        return response.status(204).send()
+
+    } catch {
+
+        return response.status(500).json({ error: 'Falha ao deletar' })
+
+    }
 }
 
 module.exports = { create, list, remove }
--- backend/src/controllers/UniversityController.js
+++ backend/src/controllers/UniversityController.js
@@ -9,24 +9,42 @@ const create = async (request, response) => {
     // Gerando um id aleatório de 2 bytes no formato string
     const id = crypto.randomBytes(2).toString('HEX')
 
-    // Inserindo dados na tabela
-    await connection('university').insert({
-        id,
-        name,
-        telephone,
-        uf,
-        city,
-        longitude,
-        latitude
-    })
+    // Verificando integridade dos dados
+    if (!name) return response.status(400).json({ error: 'Nome inválido' })
+    if (!telephone) return response.status(400).json({ error: 'Telefone inválido' })
+    if (!uf) return response.status(400).json({ error: 'UF inválido' })
+    if (!city) return response.status(400).json({ error: 'Cidade inválida' })
+    if (!longitude) return response.status(400).json({ error: 'Longitude inválida' })
+    if (!latitude) return response.status(400).json({ error: 'Latitude inválida' })
 
-    // Retornando o ID como resposta 
-    return response.json({ id })
+    try {
+
+        // Inserindo dados na tabela
+        await connection('university').insert({
+            id,
+            name,
+            telephone,
+            uf,
+            city,
+            longitude,
+            latitude
+        })
+
+        // Retornando o ID como resposta 
+        return response.json({ id })
+
+    } catch (error) {
+        return response.status(500).json({ error: 'Falha ao criar' })
+    }
 }
 
 const list = async (request, response) => {
-    const universities = await connection('university').select('*')
-    return response.json(universities)
+    try {
+        const universities = await connection('university').select('*')
+        return response.json(universities)
+    } catch (error) {
+        return response.status(500).json({ error: 'Falha ao buscar' })
+    }
 }
 
 module.exports = { create, list }
