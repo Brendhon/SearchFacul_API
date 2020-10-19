763e800ff95ac2eab085db04760ad7231f25018c
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-19 01:06:33 -0300

    add: CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
-// Importando o modulo express
-const express = require('express')
+const express = require('express') // Importando o modulo express
+const cors = require('cors')
+
 const routes = require('./routes') //Importando as rotas
 
 // Criando aplicação 
 const app = express()
 
 // Middleware
+app.use(cors()) // Permitir que as aplicações frontend possam acessar a api
 app.use(express.json()) // Ativando a utilização de JSON
 app.use(routes) // Utilizando as rotas
 
