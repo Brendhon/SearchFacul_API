85d7d8829640710efb4c9706525d46b342812e30
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-19 01:01:53 -0300

    change: Table University

--- backend/src/database/migrations/20201018191742_create_university.js
+++ backend/src/database/migrations/20201018191742_create_university.js
@@ -1,7 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('university', function (table) {
         table.string('id').primary();
-        table.string('name').notNullable();
+        table.string('universityName').notNullable();
         table.string('telephone').notNullable();
         table.string('uf', 2).notNullable();
         table.string('city').notNullable();
