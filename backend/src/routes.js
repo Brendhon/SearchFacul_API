2901a63e6b33b2e205823895ae32a499b74e6984
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-18 21:32:07 -0300

    edit: routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,36 @@
-// Importando o modulo express
-const express = require('express')
+const express = require('express') // Importando o modulo express
+const crypto = require('crypto')
+const connection = require('./database/connection')
 
+// Extraindo o modulo Rotas do express e atribuindo-o a uma variável
 const routes = express.Router()
 
-routes.get('/', (req, resp) => {
-    resp.send('hello')
+routes.get('/university', async (req, resp) => {
+    const universities = await connection('university').select('*')
+    return resp.json(universities)
 })
 
-routes.post('/', (req, resp) => {
-    resp.send(req.body)
+routes.post('/university', async (req, resp) => {
+
+    // Realizando um destruction no objeto vindo da requisição
+    const { name, telephone, uf, city, longitude, latitude } = req.body
+
+    // Gerando um id aleatório de 2 bytes no formato string
+    const id = crypto.randomBytes(2).toString('HEX')
+
+    // Inserindo dados na tabela
+    await connection('university').insert({
+        id,
+        name,
+        telephone,
+        uf,
+        city,
+        longitude,
+        latitude
+    })
+
+    // Retornando o ID como resposta 
+    return resp.json({ id }) 
 })
 
 module.exports = routes
