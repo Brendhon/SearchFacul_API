c872e790144f1ed7b978144620be06eb13ffbe4e
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-24 19:40:13 -0300

    add: function decrypt

---
+++ backend/src/utils/decrypt.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto')
+const secret = 'abcdefghijklmnopqrstuvwxyzabcdef'
+
+const decrypt = encrypted => {
+
+    const [iv, encryptedText] = encrypted.split(':')
+
+    const ivBuffer = Buffer.from(iv, 'hex') // Voltar para bytes do Hex
+    const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secret), ivBuffer)
+
+    let content = decipher.update(Buffer.from(encryptedText, 'hex'))
+    content = Buffer.concat([content, decipher.final()])
+
+    return content.toString()
+}
+
+module.exports = decrypt
