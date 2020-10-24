89dead0d9a105d0434ff720096019697cd8a4d6f
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-24 17:36:51 -0300

    remove: try-catch in SessionController.js

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -5,22 +5,15 @@ const create = async (request, response) => {
     // Realizando um destruction no objeto vindo da requisição
     const { id } = request.body
 
-    try {
+    await connection('university')
+        .where('id', id)
+        .first()
+        .then(university => {
+            if (!university) return response.status(400).json({ message: 'Universidade não encontrada' }) // Verificando se Universidade foi encontrada
+            else return response.json(university) // Retornando o objeto como resposta 
+        })
+        .catch(_ => response.status(500).json({ message: 'Falha ao criar a sessão' }))
 
-        const university = await connection('university')
-            .where('id', id)
-            .first()
-
-        if (!university) return response.status(400).json({ message: 'Universidade não encontrada' })
-
-        // Retornando o objeto como resposta 
-        return response.json(university)
-
-    } catch {
-
-        return response.status(500).json({ message: 'Falha ao criar a sessão' })
-
-    }
 }
 
 module.exports = { create }
