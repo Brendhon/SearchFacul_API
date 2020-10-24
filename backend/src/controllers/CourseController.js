455cd7efb03e31151819b08bb4f9704b69864772
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-24 19:45:27 -0300

    change: CouseController.js

--- backend/src/controllers/CourseController.js
+++ backend/src/controllers/CourseController.js
@@ -7,26 +7,28 @@ const create = async (request, response) => {
 
     // Utilizando o cabeçalho da requisição para verificar quem é o responsável por esse curso
     const university_id = request.headers.authorization
+    
+    // Verificando se faculdade foi encontrada
+    await connection('university')
+        .where('id', university_id)
+        .first()
+        .then(university => {
+            if (!university) return response.status(400).json({ message: 'Universidade não encontrada :(' })
+        })
+        .catch(_ => response.status(500).json({ message: 'Falha ao servidor' }))
 
-    try {
 
-        // Realizando um destruction no array resultado da inserção para pegar o id gerado
-        const [id] = await connection('course').insert({
+    // Realizando um destruction no array resultado da inserção para pegar o id gerado
+    await connection('course')
+        .insert({
             name,
             email,
             description,
             score,
             university_id
         })
-
-        // Retornando o ID como resposta 
-        return response.json({ id })
-
-    } catch {
-
-        return response.status(500).json({ message: 'Falha ao criar' })
-
-    }
+        .then(([id]) => response.json({ id })) // Retornando o ID como resposta 
+        .catch(_ => response.status(500).json({ message: 'Falha ao criar' }))
 }
 
 const listByName = async (request, response) => {
@@ -40,69 +42,57 @@ const listByName = async (request, response) => {
     // Numero de elementos que serão retornados
     const numElements = 5
 
-    try {
+    // Pegando o numero de cursos resultantes da busca
+    const [count] = await connection('course')
+        .where('name', 'like', `%${name}%`)
+        .count()
+        .catch(_ => response.status(500).json({ message: 'Erro no sistema' }))
 
-        // Pegando o numero de cursos resultantes da busca
-        const [count] = await connection('course')
-            .where('name', 'like', `%${name}%`)
-            .count()
+    // Buscando lista de cursos
+    await connection('course')
+        .join('university', 'university.id', '=', 'course.university_id') // Realizando um JOIN para pegar os dados da universidade
+        .limit(numElements)
+        .offset((page - 1) * numElements)
+        .where('course.name', 'like', `%${name}%`)
+        .select([
+            'course.*', //Selecionando todos os dados dos cursos 
+            'university.universityName',
+            'university.city',
+            'university.telephone',
+            'university.uf',
+            'university.street',
+            'university.number'
+        ])
+        .then(courses => {
+            response.header('X-Total-Count', count['count(*)'])
+            return response.json(courses)
+        })
+        .catch(_ => response.status(500).json({ message: 'Erro no sistema' }))
 
-        response.header('X-Total-Count', count['count(*)'])
-
-    } catch {
-        return response.status(400).json({ message: 'Curso não encontrado :(' })
-    }
-
-    try {
-        // Buscando lista de cursos
-        const courses = await connection('course')
-            .join('university', 'university.id', '=', 'course.university_id') // Realizando um JOIN para pegar os dados da universidade
-            .limit(numElements)
-            .offset((page - 1) * numElements)
-            .where('course.name', 'like', `%${name}%`)
-            .select([
-                'course.*', //Selecionando todos os dados dos cursos 
-                'university.universityName',
-                'university.city',
-                'university.telephone',
-                'university.uf',
-                'university.street',
-                'university.number'
-            ])
-
-        return response.json(courses)
-
-    } catch {
-        return response.status(400).json({ message: 'Curso não encontrado' })
-    }
 }
 
 const listById = async (request, response) => {
 
     // Pegando o Curso escolhido pelo usuário 
     const { id } = request.params
-   
-    try {
-        // Buscando lista de cursos
-        const courses = await connection('course')
-            .join('university', 'university.id', '=', 'course.university_id') // Realizando um JOIN para pegar os dados da universidade
-            .where('course.id', id)
-            .select([
-                'course.*', //Selecionando todos os dados dos cursos 
-                'university.universityName',
-                'university.city',
-                'university.telephone',
-                'university.uf',
-                'university.street',
-                'university.number'
-            ])
-            .first() // Pegando o primeiro que ele encontrar
 
-        return response.json(courses)
+    // Buscando lista de cursos
+    await connection('course')
+        .join('university', 'university.id', '=', 'course.university_id') // Realizando um JOIN para pegar os dados da universidade
+        .where('course.id', id)
+        .select([
+            'course.*', //Selecionando todos os dados dos cursos 
+            'university.universityName',
+            'university.city',
+            'university.telephone',
+            'university.uf',
+            'university.street',
+            'university.number'
+        ])
+        .first() // Pegando o primeiro que ele encontrar
+        .then(courses => response.json(courses))
+        .catch(_ => response.status(500).json({ message: 'Erro no sistema' }))
 
-    } catch {
-        return response.status(400).json({ message: 'Curso não encontrado' })
-    }
 }
 
 const remove = async (request, response) => {
@@ -113,35 +103,22 @@ const remove = async (request, response) => {
     // Utilizando o cabeçalho da requisição para verificar quem é o responsável por esse curso
     const university_id = request.headers.authorization
 
-    try {
+    // Verificando se o ID da requisição é o mesmo ID do responsável pelo curso (EVITAR QUE UMA UNIVERSIDADE EXCLUA O CURSO DE OUTRA)
+    await connection('course')
+        .where('id', id)
+        .select('university_id')
+        .first()
+        .then(course => {
+            if (university_id != course.university_id) return response.status(401).json({ message: 'Operação não permitida' })
+        })
+        .catch(_ => response.status(500).json({ message: 'Falha no Sistema' }))
 
-        const courses = await connection('course')
-            .where('id', id)  // Comparando o ID escolhido com o do banco
-            .select('university_id') // Pegando o ID da universidade responsável
-            .first() // Pegando o primeiro que ele encontrar
+    await connection('course')
+        .where('id', id)  // Comparando o ID escolhido com o do banco
+        .delete() // Deletando o curso no banco
+        .then(_ => response.status(204).send())
+        .catch(_ => response.status(500).json({ message: 'Falha ao deletar' }))
 
-        // Verificando se o ID da requisição é o mesmo ID do responsável pelo curso (EVITAR QUE UMA UNIVERSIDADE EXCLUA O CURSO DE OUTRA)
-        if (university_id != courses.university_id) return response.status(401).json({ message: 'Operação não permitida' })
-
-    } catch {
-
-        return response.status(400).json({ message: 'Falha ao buscar' })
-
-    }
-
-    try {
-
-        await connection('course')
-            .where('id', id)  // Comparando o ID escolhido com o do banco
-            .delete() // Deletando o curso no banco
-
-        return response.status(204).send()
-
-    } catch {
-
-        return response.status(500).json({ message: 'Falha ao deletar' })
-
-    }
 }
 
 module.exports = { create, listByName, listById, remove }
