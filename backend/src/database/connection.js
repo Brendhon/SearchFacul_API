ab647467e2732eee63afb38a66d86a1ebb51099a
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-26 18:02:28 -0300

    add config: test database

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -4,8 +4,13 @@ const knex = require('knex')
 //importando as configurações do arquivo knex e atribuindo a uma variável
 const configuration = require('../../knexfile')
 
-//Realizando a conexão no modo desenvolvimento
-const connection = knex(configuration.development)
+// Pegando a variável ambiente criada pelo cross-env
+const env = process.env.NODE_ENV 
 
+// Verificando se a variável ambiente esta no modo teste ou desenvolvedor
+const config = env == 'test' ? configuration.test : configuration.development
+
+//Realizando a conexão no modo desenvolvimento
+const connection = knex(config)
 
 module.exports = connection
