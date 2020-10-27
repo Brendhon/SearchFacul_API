767860f644579d822ec2ca21eaa14552c51ce8ae
Brendhon <brendhon.e.c.m@gmail.com>
2020-10-26 21:02:15 -0300

    test: Create University

---
+++ backend/__tests__/integration/UniversityController.spec.js
@@ -0,0 +1,32 @@
+const request = require('supertest')
+const app = require('../../src/app')
+const connection = require('../../src/database/connection')
+
+describe("University", () => {
+
+    beforeAll(async () => {
+        await connection.migrate.latest() // Executa os migrates antes de dos testes serem chamados
+    })
+
+    afterAll(async () => {
+        await connection.destroy() // Apos TODOS os testes serem executados destrua a conexão
+    });
+
+    it("Should be able to create a new University", async () => {
+        const response = await request(app)
+            .post('/university')
+            .send({
+                universityName: "Inatel",
+                telephone: "34546789",
+                uf: "MG",
+                city: "Santa Rita",
+                street: "Sei não",
+                number: 70,
+                site: "https://inatel.br/home/"
+            })
+
+        expect(response.body).toHaveProperty('id')
+        expect(response.body.id).toHaveLength(8);
+    })
+
+})
