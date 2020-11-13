801b8605c34aa5690845d376531e1b93f1edd235
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-13 20:00:45 -0300

    remove: unused reference

--- __tests__/integration/University/UniversityControllerList.spec.js
+++ __tests__/integration/University/UniversityControllerList.spec.js
@@ -1,7 +1,6 @@
 const request = require('supertest')
 const app = require('../../../src/app')
 const connection = require('../../../src/database/connection')
-const encrypt = require('../../utils/encrypt')
 
 describe("University", () => {
 
