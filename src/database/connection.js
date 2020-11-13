9f30ad7e06cfe785ba11a0f6a9a3a12f90c588a3
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-13 20:33:07 -0300

    add: create migrates

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,7 @@
-//Importando o Knex
+// Importando o Knex
 const knex = require('knex')
 
-//importando as configurações do arquivo knex e atribuindo a uma variável
+// Importando as configurações do arquivo knex e atribuindo a uma variável
 const configuration = require('../../knexfile')
 
 // Pegando a variável ambiente criada pelo cross-env
@@ -10,7 +10,10 @@ const env = process.env.NODE_ENV
 // Verificando se a variável ambiente esta no modo teste ou desenvolvedor
 const config = env == 'test' ? configuration.test : configuration.development
 
-//Realizando a conexão no modo desenvolvimento
+// Realizando a conexão no modo desenvolvimento
 const connection = knex(config)
+
+// Executando o comando de rodar as migrations pelo código
+connection.migrate.latest([config]) 
 
 module.exports = connection
