6f681e96e638b923c2c43a0dea2dfbbba45c3cfd
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-13 23:49:53 -0300

    refact: Update course

--- __tests__/integration/Course/CourseControllerUpdate.spec.js
+++ __tests__/integration/Course/CourseControllerUpdate.spec.js
@@ -1,11 +1,15 @@
 const request = require('supertest')
 const app = require('../../../src/app')
 const connection = require('../../../src/database/connection')
+const CONSTANTS = require('../../utils/constants')
 
 describe("Course", () => {
 
+    // Atributos
     let token
+    let courseExample
 
+    // Antes de todos execute...
     beforeAll(async () => {
 
         await connection.migrate.rollback() // Realiza um rollback para evitar que o banco cresça sem controle
@@ -14,25 +18,12 @@ describe("Course", () => {
         // Inserindo um dado no banco como teste
         await request(app)
             .post('/university')
-            .send({
-                IES: "Inatel",
-                telephone: "34546789",
-                uf: "MG",
-                email: "guilherme@gmail.br",
-                password: "123",
-                city: "Santa Rita",
-                address: "Sei não",
-                category: "privada",
-                site: "https://inatel.br/home/"
-            })
+            .send(CONSTANTS.universityExample)
 
         // Realizando o login
         const response = await request(app)
             .post('/session')
-            .send({
-                email: "guilherme@gmail.br",
-                password: "123"
-            })
+            .send(CONSTANTS.loginExample)
 
         token = response.body.token // Pegando o ID resultante da resposta
 
@@ -40,17 +31,16 @@ describe("Course", () => {
         await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(CONSTANTS.courseExample)
 
     })
 
+    // Antes de cada execute...
+    beforeEach(() => {
+        courseExample = Object.assign({}, CONSTANTS.courseExample2) // Clonando o objetivo para não mexer na variável original
+    })
+
+    // Depois de todos execute...
     afterAll(async () => {
         await connection.destroy() // Apos TODOS os testes serem executados destrua a conexão
     })
@@ -59,14 +49,7 @@ describe("Course", () => {
         const response = await request(app)
             .put('/course/1')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de Produção",
-                description: "ADM com CREA",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.status).toBe(204)
     })
@@ -74,14 +57,7 @@ describe("Course", () => {
     it("Shouldn't be able to update a Course without authorization", async () => {
         const response = await request(app)
             .put('/course/1')
-            .send({
-                name: "Engenharia de Produção",
-                description: "ADM com CREA",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.body.statusCode).toBe(400)
     })
@@ -90,14 +66,7 @@ describe("Course", () => {
         const response = await request(app)
             .put('/course/1')
             .set("Authorization", '12345678')
-            .send({
-                name: "Engenharia de Produção",
-                description: "ADM com CREA",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.status).toBe(400)
     })
