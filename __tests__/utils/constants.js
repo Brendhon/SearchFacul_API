441f6fadbaf6638bfc62c30ed8ece443450556cc
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-13 23:37:33 -0300

    create: constants file

---
+++ __tests__/utils/constants.js
@@ -0,0 +1,27 @@
+const universityExample = {
+    IES: "Inatel",
+    telephone: "34546789",
+    email: "guilherme@gmail.br",
+    password: "123",
+    uf: "MG",
+    city: "Santa Rita",
+    address: "Sei não",
+    category: "privada",
+    site: "https://inatel.br/home/"
+}
+
+const courseExample = {
+    name: "Engenharia de computação",
+    description: "Melhor Curso",
+    duration: "5 anos",
+    titration: "Bacharelado",
+    modality: "Presencial",
+    score: 4
+}
+
+const loginExample = {
+    email: "guilherme@gmail.br",
+    password: "123"
+}
+
+module.exports = { universityExample, courseExample, loginExample }
