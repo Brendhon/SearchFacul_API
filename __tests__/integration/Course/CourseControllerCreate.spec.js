fb6d85bf5bec6116d2e47edefa056e07df990857
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-13 23:38:47 -0300

    refact: constants

--- __tests__/integration/Course/CourseControllerCreate.spec.js
+++ __tests__/integration/Course/CourseControllerCreate.spec.js
@@ -1,11 +1,15 @@
 const request = require('supertest')
 const app = require('../../../src/app')
 const connection = require('../../../src/database/connection')
+const CONSTANTS = require('../../utils/constants')
 
 describe("Course", () => {
 
+    // Atributos
     let token
+    let courseExample
 
+    // Antes de todos execute...
     beforeAll(async () => {
 
         await connection.migrate.rollback() // Realiza um rollback para evitar que o banco cresça sem controle
@@ -14,29 +18,22 @@ describe("Course", () => {
         // Inserindo um dado no banco como teste
         await request(app)
             .post('/university')
-            .send({
-                IES: "Inatel",
-                telephone: "34546789",
-                email: "guilherme@gmail.br",
-                password: "123",
-                uf: "MG",
-                city: "Santa Rita",
-                address: "Sei não",
-                category: "privada",
-                site: "https://inatel.br/home/"
-            })
+            .send(CONSTANTS.universityExample)
 
         const response = await request(app)
             .post('/session')
-            .send({
-                email: "guilherme@gmail.br",
-                password: "123"
-            })
+            .send(CONSTANTS.loginExample)
 
         token = response.body.token // Pegando o ID resultante da resposta
 
     })
 
+    // Antes de cada execute...
+    beforeEach(() => {
+        courseExample = Object.assign({}, CONSTANTS.courseExample) // Clonando o objetivo para não mexer na variável original
+    })
+
+    // Depois de todos execute...
     afterAll(async () => {
         await connection.destroy() // Apos TODOS os testes serem executados destrua a conexão
     })
@@ -45,14 +42,7 @@ describe("Course", () => {
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.body).toHaveProperty('id')
     })
@@ -60,14 +50,7 @@ describe("Course", () => {
     it("Shouldn't be able to create a new Course without authorization", async () => {
         const response = await request(app)
             .post('/course')
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.body.statusCode).toBe(400)
     })
@@ -76,125 +59,97 @@ describe("Course", () => {
         const response = await request(app)
             .post('/course')
             .set("Authorization", '12345678')
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.status).toBe(400)
     })
 
     it("Shouldn't be able to create a new Course without name", async () => {
+
+        delete courseExample["name"]  // Removendo atributo desejado
+
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["name"])
     })
 
     it("Shouldn't be able to create a new Course without description", async () => {
+
+        delete courseExample["description"]  // Removendo atributo desejado
+
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
+
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["description"])
     })
 
     it("Shouldn't be able to create a new Course without duration", async () => {
+
+        delete courseExample["duration"]  // Removendo atributo desejado
+
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["duration"])
     })
 
     it("Shouldn't be able to create a new Course without titration", async () => {
+
+        delete courseExample["titration"]  // Removendo atributo desejado
+
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                modality: "Presencial",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["titration"])
     })
 
     it("Shouldn't be able to create a new Course without modality", async () => {
+
+        delete courseExample["modality"]  // Removendo atributo desejado
+
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                score: 4
-            })
+            .send(courseExample)
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["modality"])
     })
 
     it("Should be able to create a new Course without score", async () => {
+
+        delete courseExample["score"]  // Removendo atributo desejado
+
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial"
-            })
+            .send(courseExample)
 
         expect(response.body).toHaveProperty('id')
     })
 
     it("Shouldn't be able to create a new Course with incorrect score", async () => {
+
+        courseExample.score = 6  // Atribuindo um valor inválido
+
         const response = await request(app)
             .post('/course')
             .set("Authorization", token)
-            .send({
-                name: "Engenharia de computação",
-                description: "Melhor Curso",
-                duration: "5 anos",
-                titration: "Bacharelado",
-                modality: "Presencial",
-                score: 6
-            })
+            .send(courseExample)
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["score"])
