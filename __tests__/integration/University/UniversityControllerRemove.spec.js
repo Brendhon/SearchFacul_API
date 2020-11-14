ecf43f914bef8697cd2460d5764099550e09a51d
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-14 00:22:29 -0300

    refact: Remove university

--- __tests__/integration/University/UniversityControllerRemove.spec.js
+++ __tests__/integration/University/UniversityControllerRemove.spec.js
@@ -1,41 +1,37 @@
 const request = require('supertest')
 const app = require('../../../src/app')
 const connection = require('../../../src/database/connection')
+const CONSTANTS = require('../../utils/constants')
 
 describe("University", () => {
 
+    // Atributos
     let token
 
+    // Antes de todos execute...
     beforeAll(async () => {
 
         await connection.migrate.rollback() // Realiza um rollback para evitar que o banco cresça sem controle
         await connection.migrate.latest() // Executa os migrates antes de dos testes serem chamados
+    })
+
+    // Antes de cada execute...
+    beforeEach(async () => {
 
         // Inserindo um dado no banco como teste
         await request(app)
             .post('/university')
-            .send({
-                IES: "Inatel",
-                telephone: "34546789",
-                email: "guilherme@gmail.br",
-                password: "123",
-                uf: "MG",
-                city: "Santa Rita",
-                address: "Sei não",
-                category: "privada",
-                site: "https://inatel.br/home/"
-            })
+            .send(CONSTANTS.universityExample)
 
         const response = await request(app)
             .post('/session')
-            .send({
-                email: "guilherme@gmail.br",
-                password: "123"
-            })
+            .send(CONSTANTS.loginExample)
 
-        token = response.body.token // Pegando o ID resultante da resposta
+        token = response.body.token // Pegando o ID resultante da resposta  
+
     })
 
+    // Depois de todos execute...
     afterAll(async () => {
         await connection.destroy() // Apos TODOS os testes serem executados destrua a conexão
     })
