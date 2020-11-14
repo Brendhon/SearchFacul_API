0144270610124dcb12081f9dab4a935321e552c7
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-14 00:01:02 -0300

    refact: Create session

--- __tests__/integration/Session/SessionController.spec.js
+++ __tests__/integration/Session/SessionController.spec.js
@@ -1,30 +1,31 @@
 const request = require('supertest')
 const app = require('../../../src/app')
 const connection = require('../../../src/database/connection')
+const CONSTANTS = require('../../utils/constants')
 
 describe("Session", () => {
 
+    // Atributos
+    let loginExample
+
+    // Antes de todos execute...
     beforeAll(async () => {
 
         await connection.migrate.rollback() // Realiza um rollback para evitar que o banco cresça sem controle
         await connection.migrate.latest() // Executa os migrates antes de dos testes serem chamados
 
-        // Inserindo um dado no banco como teste
+        // Inserindo uma universidade no banco como teste
         await request(app)
             .post('/university')
-            .send({
-                IES: "Inatel",
-                telephone: "34546789",
-                email: "guilherme@gmail.br",
-                password: "123",
-                uf: "MG",
-                city: "Santa Rita",
-                address: "Sei não",
-                category: "privada",
-                site: "https://inatel.br/home/"
-            })
+            .send(CONSTANTS.universityExample)
     })
 
+    // Antes de cada execute...
+    beforeEach(() => {
+        loginExample = Object.assign({}, CONSTANTS.loginExample) // Clonando o objetivo para não mexer na variável original
+    })
+
+    // Depois de todos execute...
     afterAll(async () => {
         await connection.destroy() // Apos TODOS os testes serem executados destrua a conexão
     })
@@ -33,21 +34,18 @@ describe("Session", () => {
 
         const response = await request(app)
             .post('/session')
-            .send({
-                email: "guilherme@gmail.br",
-                password: "123"
-            })
+            .send(loginExample)
 
         expect(response.body.token).toBeTruthy()
     })
 
     it("Shouldn't be able to create a new session without email", async () => {
 
+        delete loginExample["email"]
+
         const response = await request(app)
             .post('/session')
-            .send({
-                password: "123"
-            })
+            .send(loginExample)
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["email"])
@@ -55,11 +53,11 @@ describe("Session", () => {
 
     it("Shouldn't be able to create a new session with a without password", async () => {
 
+        delete loginExample["password"]
+
         const response = await request(app)
             .post('/session')
-            .send({
-                email: "guilherme@gmail.br",
-            })
+            .send(loginExample)
 
         expect(response.body.statusCode).toBe(400)
         expect(response.body.validation.body.keys).toEqual(["password"])
@@ -67,12 +65,11 @@ describe("Session", () => {
 
     it("Shouldn't be able to create a new session with incorrect password", async () => {
 
+        loginExample.password = 456
+
         const response = await request(app)
             .post('/session')
-            .send({
-                email: "guilherme@gmail.br",
-                password: "456"
-            })
+            .send(loginExample)
 
         expect(response.status).toBe(400)
     })
