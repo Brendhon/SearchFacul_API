cd3f3fc1994a54fea5b7d2c7baf49e84ab30c824
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-26 16:59:13 -0300

    change: return

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,16 +1,22 @@
 //Importando a configuração de desenvolvimento
 const connection = require('../database/connection')
+const CONSTANTS = require('../utils/constants')
 
 const list = async (request, response) => {
 
     // Utilizando o cabeçalho da requisição para verificar quem é o responsável por esse curso
     const { university_id } = request
 
-    // Buscando lista de cursos referente a uma faculdade especifica 
+    // Buscando lista de cursos
     await connection('course')
-        .where('university_id', university_id)
-        .then(courses => response.json(courses))
-        .catch(_ => response.status(500).json({ message: 'Falha no sistema' }))
+        .join('university', 'university.id', '=', 'course.university_id') // Realizando um JOIN para pegar os dados da universidade
+        .where('university.id', university_id)
+        .select(CONSTANTS.universityAndCourseData)
+        .then(courses => {
+            response.header('X-Total-Count', courses.length)
+            return response.json(courses)
+        })
+        .catch(_ => response.status(500).json({ message: 'Erro no sistema' }))
 }
 
 module.exports = { list }
