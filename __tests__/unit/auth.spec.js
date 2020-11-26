4a468e62e2229c5dd551361cceb6cd59ba999d79
Brendhon <brendhon.e.c.m@gmail.com>
2020-11-26 19:58:28 -0300

    test: comparePassword

---
+++ __tests__/unit/auth.spec.js
@@ -0,0 +1,27 @@
+const { encryptPassword, comparePassword  } = require('../../src/utils/auth')
+
+describe("Auth", () => {
+
+    let password
+    let samePassword
+
+    // Antes de todos execute...
+    beforeAll(async () => {
+        password = await encryptPassword("123")
+        samePassword = false
+    })
+
+    it("Should be able to recognize a password", async () => {
+
+        samePassword = await comparePassword("123", password)
+
+        expect(samePassword).toBeTruthy()
+    })
+
+    it("Shouldn't be able to recognize a password", async () => {
+
+        samePassword = await comparePassword("1234", password)
+
+        expect(samePassword).toBeFalsy();
+    })
+})
